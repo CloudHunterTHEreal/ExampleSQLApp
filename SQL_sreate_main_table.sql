a256f8a3041b6219e1cd55f3f1e75db854266bc7
Andrey Martynov <2web@list.ru>
2023-02-04 11:21:18 +0300

    Start SQL learning

---
+++ SQL_sreate_main_table.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE examples(
+	lastname varchar(5) NOT NULL,
+	firstname varchar(5),
+	middlename varchar(5),
+	birthday DATE NOT NULL,
+	sex varchar(1) NOT NULL,
+	id int NOT NULL,
+	payment_mode varchar(15),
+	fault_tests int,
+	comment varchar(250),
+	PRIMARY KEY (id)
+);
